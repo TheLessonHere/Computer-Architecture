f7b76ce51132fe4db21c8bf92af74e90cf86866d
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2020-01-29 14:18:06 -0700

    Day 3 completed, implemented stack

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -38,6 +38,8 @@ ST = 0b10000100
 SUB = 0b10100001
 XOR = 0b10101011
 
+# Create a Stack Pointer
+SP = 7
 
 class CPU:
     """Main CPU class."""
@@ -46,14 +48,14 @@ class CPU:
         """Construct a new CPU."""
         # General purpose registers
         self.reg = [0] * 8
-        # Set the reg at index 7 to the keystroke instruction
-        self.reg[7] = 0xFF
         # 256 bit ram
         self.ram = [0] * 256
         # Program Counter
         self.pc = 0
         # Flags
         self.fl = 0
+        # Set the reg at index 7 to position 0xF4 by default
+        self.reg[SP] = 0xF4
         # Keep track of if the CPU is running for the halt function
         self.running = False
 
@@ -67,16 +69,15 @@ class CPU:
         with open('examples/' + file_to_run, 'r') as f:
             # For each command...
             for line in f:
-                # Ignore comments
-                if line.startswith('#') or line.startswith('\n'):
+                # Grab the command out of the file while ignoring comments
+                command = line.split('#')[0]
+                command = command.strip()
+                if command == "":
                     continue
-                else:
-                    # Grab the command out of the file
-                    command = line.split(' ')[0]
-                    # Convert binary command string to int value and load it into the ram
-                    self.ram[address] = int(command, 2)
-                    # increment the register address for the next command
-                    address += 1
+                # Convert binary command string to int value and load it into the ram
+                self.ram[address] = int(command, 2)
+                # increment the register address for the next command
+                address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -139,6 +140,26 @@ class CPU:
         print(self.reg[reg_num])
         self.pc += 2
 
+    def handle_push(self, reg_num):
+        # Decrement the stack pointer
+        self.reg[SP] -= 1
+        # Geth the value to be pushed
+        value = self.reg[reg_num]
+        # Set the ram at the registered index to the value
+        self.ram[self.reg[SP]] = value
+        # Increment the pc forward
+        self.pc += 2
+
+    def handle_pop(self, reg_num):
+        # Store the value at the top of the stack
+        value = self.ram[self.reg[SP]]
+        # Set the reg at the correct index equal to the popped value
+        self.reg[reg_num] = value
+        # Increment the stack pointer
+        self.reg[SP] += 1
+        # Increment the pc forward
+        self.pc += 2
+
     def run(self):
         """Run the CPU."""
         self.running = True
@@ -159,6 +180,10 @@ class CPU:
             elif ir == MUL:
                 self.alu("MUL", operand_a, operand_b)
                 self.pc += 3
+            elif ir == PUSH:
+                self.handle_push(operand_a)
+            elif ir == POP:
+                self.handle_pop(operand_a)
             else:
                 print("Instruction not found, exiting program with error")
                 sys.exit(1)
